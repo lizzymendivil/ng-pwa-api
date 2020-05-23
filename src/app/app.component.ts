f46e36b8a77bfc0f258e0744a3c1cc38f22f9e1c
can3lita <lizzy.mb@gmail.com>
2020-05-22 21:10:05 -0400

    Run pwa command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-api';
+  apiData: any;
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(
+      (res: any) => {
+        this.apiData = res;
+      }, err => {
+        console.error(err);
+      }
+    )
+  }
 }
